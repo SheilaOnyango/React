70fedfe1f3ed0152b8eaa545fd7df2c1aa3bbd32
sheilaonyango <edusheila31@gmail.com>
2024-06-10 12:26:51 +0300

    Add props to the CoreConcept component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from "./CoreConcept";
 import Header from "./Header";
+import componentsImg from "./assets/components.png";
 
 function App() {
   return (
@@ -9,7 +10,11 @@ function App() {
         <section id="core-concepts"></section>
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept />
+          <CoreConcept
+            title="Components"
+            description="The core UI building block."
+            image={componentsImg}
+          />
           <CoreConcept />
           <CoreConcept />
           <CoreConcept />
