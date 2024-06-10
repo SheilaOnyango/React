3fa86fbf496bd965eacead99d958d116dbde03e4
sheilaonyango <edusheila31@gmail.com>
2024-06-10 12:12:20 +0300

    Add a new component

---
+++ src/CoreConcept.jsx
@@ -0,0 +1,11 @@
+const CoreConcept = () => {
+  return (
+    <li>
+      <img src="..." alt="..." />
+      <h3>Title</h3>
+      <p>Description</p>
+    </li>
+  );
+};
+
+export default CoreConcept;
